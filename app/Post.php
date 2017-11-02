4566c87ab13c0c72476ba9c97551d989ee8375e7
i65 <2461585309@qq.com>
2017-11-02 15:57:22 +0800

    使用全局scope进行软删除模型

--- app/Post.php
+++ app/Post.php
@@ -3,9 +3,21 @@
 namespace App;
 
 use App\Model;
+use Illuminate\Database\Eloquent\Builder;
+
 
 //对应表-->posts
 class Post extends Model
 {
 
+
+    //全局scope方式
+    protected static function boot(){
+
+        parent::boot();
+
+        static::addGlobalScope('avaible', function(Builder $builder){
+            $builder->whereIn('status', [0,1]);
+        });
+    }
 }
