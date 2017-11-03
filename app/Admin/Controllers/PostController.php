df5b14a9835ecfdf2ff4aaf4ca896480c612b590
i65 <2461585309@qq.com>
2017-11-03 10:01:15 +0800

    使用withoutGlobalScope来完成文章审核模块

--- app/Admin/Controllers/PostController.php
+++ app/Admin/Controllers/PostController.php
@@ -2,17 +2,31 @@
 
 namespace App\Admin\Controllers;
 
-use \App\Post;
+use App\Post;
 
 class PostController extends Controller
 {
     //首页
     public function index(){
-        return view('admin.post.index');
+
+        $posts = Post::withoutGlobalScope('avaible')->where('status', 0)->orderBy('created_at', 'desc')->paginate(10);
+
+        return view('admin.post.index', compact('posts'));
     }
 
     public function status(Post $post){
-        //TODO...
-        
+
+        $this->validate(request(), [
+           'status' => 'required|in: -1,1',
+        ]);
+
+        $post->status = request('status');
+        $post->id = request('id');
+        $post->update();
+
+        return [
+            'error' => 0,
+            'msg' => ''
+        ];
     }
 }
--- public/js/admin.js
+++ public/js/admin.js
@@ -1,3 +1,32 @@
 /**
  * Created by Administrator on 2017/11/2.
  */
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN' : $('meta[name="csrf-token"]').attr('content')
+    }
+});
+$('.post-audit').click(function(event){
+   var target = $(event.target);
+   var post_id = target.attr('post-id');
+   var status = target.attr('post-action-status');
+
+   $.ajax({
+      url : '/admin/posts/' + post_id + '/status',
+       method : 'POST',
+       data : { 'status' : status, 'id' : post_id },
+       dataType : 'json',
+       success : function(data){
+          alert(data);
+          if(data.error != 0){
+              alert(data.msg);
+              return;
+          }
+          target.parent().parent().remove();
+       },
+       error:function(err){
+           console.log(err);
+       }
+
+   });
+});
