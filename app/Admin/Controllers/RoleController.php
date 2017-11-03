200cfe94ffed088f2eb34bbb1af521c7a288fdd6
i65 <2461585309@qq.com>
2017-11-03 14:43:50 +0800

    搭建路由和页面模板

---
+++ app/Admin/Controllers/RoleController.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Admin\Controllers;
+
+class RoleController extends Controller
+{
+    //角色列表
+    public function index(){
+        return view('admin.role.index');
+    }
+
+    //创建角色
+    public function create(){
+        return view('admin.role.add');
+    }
+
+    //创建角色行为
+    public function store(){
+
+    }
+
+    //角色和权限关系页面
+    public function permission(){
+        return view('admin.role.permission');
+    }
+
+    //储存角色权限行为
+    public function storePermission(){
+
+    }
+}
--- app/Admin/Controllers/UserController.php
+++ app/Admin/Controllers/UserController.php
@@ -32,4 +32,14 @@ class UserController extends Controller
         //渲染
         return redirect('/admin/users');
     }
+
+    //用户角色页面
+    public function role(){
+        return view('admin.user.role');
+    }
+
+    //储存用户角色
+    public function storeRole(){
+
+    }
 }
